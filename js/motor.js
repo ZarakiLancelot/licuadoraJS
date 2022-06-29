f78d9f77dbf70571af730d8a0dfd0c18411120db
ZarakiLancelot <kenpachi2783@gmail.com>
2022-06-29 03:42:14 -0600

    Sonidos para la licuadora

--- js/motor.js
+++ js/motor.js
@@ -1,7 +1,13 @@
 let estadoLicuadora = "apagada";
 let licuadora = document.getElementById('blender');
+let sonido_licuadora = document.getElementById('blender-sound');
+let boton_licuadora = document.getElementById('blender-button-sound');
 
 function controlarLicuadora() {
     estadoLicuadora == "apagada" ? (estadoLicuadora = "encendida", licuadora.classList.add("active")) : (estadoLicuadora = "apagada", licuadora.classList.remove("active"));
     console.log(estadoLicuadora);
+}
+
+function sonidosLicuadora() {
+    sonido_licuadora.paused ? (boton_licuadora.play(), sonido_licuadora.play()) : (boton_licuadora.play(), sonido_licuadora.pause(), sonido_licuadora.currentTime = 0);
 }
