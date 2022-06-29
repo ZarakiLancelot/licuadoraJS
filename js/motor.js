67abffa55342a0a11c2dc34941cbd2e5d5c1dd26
ZarakiLancelot <kenpachi2783@gmail.com>
2022-06-29 03:26:57 -0600

    Se incluyen assets y se genera la animación para la licuadora

--- js/motor.js
+++ js/motor.js
@@ -2,6 +2,6 @@ let estadoLicuadora = "apagada";
 let licuadora = document.getElementById('blender');
 
 function controlarLicuadora() {
-    estadoLicuadora == "apagada" ? estadoLicuadora = "encendida" : estadoLicuadora = "apagada";
+    estadoLicuadora == "apagada" ? (estadoLicuadora = "encendida", licuadora.classList.add("active")) : (estadoLicuadora = "apagada", licuadora.classList.remove("active"));
     console.log(estadoLicuadora);
 }
